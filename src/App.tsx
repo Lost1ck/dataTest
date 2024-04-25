e856c2303e2e446211a0d46390e256741f224174
LostOne <48584533+Lost1ck@users.noreply.github.com>
2024-04-25 20:46:07 +0200

    tried useEffect + localStorage in useState, jsut base things

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { people } from './data/data';
 import { Human } from './data/data.interface';
 
+interface ILocalStoreGet {
+  count: number | null;
+}
+
 function App() {
   const [ count, setCount ] = useState(0);
 
-  const handleClick = () => {
-    setTimeout(() => {
-      console.log(count);
-    }, 3000)
+  useEffect(() => {
+    const localStoreGet: ILocalStoreGet = localStorage.getItem('localState');
+    const parsedState = JSON.parse(localStoreGet);
+    setCount(parsedState);
+    console.log('get: ' + parsedState)
+  },[])
+
+  useEffect(() => {
+    if(count !== 0) localStorage.setItem('localState', JSON.stringify(count));
+  }, [count])
+
+  const debounce = (func: Function, delay: number) => {
+    let timerId: NodeJS.Timeout;
+    return function (...args: any[]) {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => func.apply(this, args), delay);
+    }
+  };
+
+  const handleClick = debounce(() => {
+    console.log('yes');
+  }, 3000)
+
+  const handleClickWithCount = () => {
+    handleClick();
+    setCount((prev) => prev + 1);
   }
 
   const peopleCount = (data: Human[]) => {
@@ -22,11 +48,11 @@ function App() {
   return (
       <>
       <ul>
-      {peopleCount(people).map((item, index) => {
-        return <li key={index}>{item}</li>;
-      })}
-    </ul>
-      <button onClick={() => {handleClick(); setCount((prev) => prev + 1)}}>
+        {peopleCount(people).map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+      <button onClick={handleClickWithCount}>
           {count}
       </button>
       </>
